1665eba2a16dbffc3b381323115210487f0efcb8
JCELIS04 <joshuacelis4@gmail.com>
2022-06-07 06:59:16 -0500

    DISEÑO

--- PROYECTO_DES.py
+++ PROYECTO_DES.py
@@ -1,17 +1,14 @@
 import base64
 import pyDes
 
-Rta = int(input("Bienvenido \n\n 1. Cifrar \n2. Descifrar \n\n Ruta: "))
+Rta = int(input("Bienvenido \n\n 1. Cifrar \n 2. Descifrar \n\n Opcion: "))
 if Rta == 1:
-  archivo=input("Infrgrese el nombre del archivo a cifrar: ")
+  archivo=input("Ingrese el nombre del archivo a cifrar: ")
   with open(archivo, "rb") as img_file:
     b64image = base64.b64encode(img_file.read())
 
   key=input("Ingrese la clave: ")
   print("Archivo en base64:", b64image)
-
- 
-  #key = "DESCRYPT" 
 
   k = pyDes.des(key.encode(), pyDes.CBC,b"\0\1\0\1\0\1\0\0",pad=None, padmode=pyDes.PAD_PKCS5)
   encriptada = k.encrypt(b64image)
